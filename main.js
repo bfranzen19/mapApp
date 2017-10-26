3f8916cdfb06db164092c0d759c1a0a6b13196b6
bt <btfranzen@gmail.com>
2017-10-26 15:44:26 -0600

    points rendered on the screen

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 var mainVM = new Vue({
   el: '#app', // this VM is mounted to the element with the ID '#app'
   data: {
-    map: '/coMap.jpg'
+    points: []
 
 
 
   }, // end of the data object
 
   methods: {
-
-
+    makePoints: function(event) {
+      console.log(event.offsetX, event.offsetY),
+      this.points.push({
+        pointX: event.offsetX+10.5 + 'px',
+        pointY: event.offsetY-3 + 'px',
+      })
+    },
+    removePoints: function(event) {
+      console.log('removed')
+      
+    }
 
 
 
