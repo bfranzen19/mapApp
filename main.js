c3e911c5011cd9d31f8edd646017a395705754b7
bt <btfranzen@gmail.com>
2017-10-26 13:48:03 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+var mainVM = new Vue({
+  el: '#app', // this VM is mounted to the element with the ID '#app'
+  data: {
+    map: '/coMap.jpg'
+
+
+
+  }, // end of the data object
+
+  methods: {
+
+
+
+
+
+
+  }, // end of the methods object
+
+}) // end of mainVM
